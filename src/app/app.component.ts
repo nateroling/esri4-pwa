247294623f231f7a69664267c72e9f3eebfd796a
Nate Roling <roling@cartegraph.com>
2022-03-10 09:34:55 -0800

    Show me a map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'esri4-pwa';
+
+  ngOnInit() {
+      var map = new Map({
+        basemap: "topo-vector"
+      });
+
+      const view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center: [-118.80500, 34.02700], // longitude, latitude
+      zoom: 13
+      });
+
+      console.log(view);
+
+  }
 }
